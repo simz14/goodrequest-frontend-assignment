64f2d3f27da383e85f2a63260fdfca284a6bc99f
simz14 <szozulakova@gmail.com>
2026-08-25 21:56:47 +0200

    chore: configure styled-components

---
+++ src/app/page.styles.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+`;
+
+export const Text = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+  letter-spacing: -0.5px;
+  color: crimson;
+`;
